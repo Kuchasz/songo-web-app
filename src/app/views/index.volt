5b3c5bce72d2a53dfd43add6bcaedddae02a0e20
Rafal Pysz <kuchasz1@o2.pl>
2018-09-22 12:39:42 +0200

    fixed page title

--- src/app/views/index.volt
+++ src/app/views/index.volt
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<title>Alpha by HTML5 UP</title>
+	<title>Songo and zuza - profesjonalna obsługa muzyczna imprez</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	<link rel="stylesheet" href="/assets/css/lightbox.min.css" />
--- src/cache/_var_www_html_app_views_index.volt_e_.php
+++ src/cache/_var_www_html_app_views_index.volt_e_.php
@@ -1,8 +1,8 @@
-a:11:{i:0;s:332:"<!DOCTYPE HTML>
+a:11:{i:0;s:419:"<!DOCTYPE HTML>
 <html>
 
 <head>
-	<title>Alpha by HTML5 UP</title>
+	<title>Songo and zuza - profesjonalna obsługa muzyczna imprez okoliczno¶ciowych i wesel - Andrychów, Kraków</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	<link rel="stylesheet" href="/assets/css/lightbox.min.css" />
